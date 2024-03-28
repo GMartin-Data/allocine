0224af857a5b1bd17508b63906731d45052cd164
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-28 14:12:20 +0100

    Implement pagination

--- allocine/allocine/spiders/ac.py
+++ allocine/allocine/spiders/ac.py
@@ -3,13 +3,14 @@ import re
 import scrapy
 
 
+BASE_URL = "https://allocine.fr"
+
 class AcSpider(scrapy.Spider):
     name = "ac"
     allowed_domains = ['allocine.fr']
     start_urls = ['https://www.allocine.fr/films/']
 
     def parse(self, response):
-        BASE_URL = "https://allocine.fr"
         # Extract films links
         film_links = response.css("h2.meta-title a::attr(href)").getall()
 
@@ -27,4 +28,20 @@ class AcSpider(scrapy.Spider):
             yield response.follow(box_office_page_url, self.parse_box_office_page, meta={'film_id': film_id})
 
         # Handle pagination
-        pass
+        next_page_url = self.get_next_page_url(response)
+        if next_page_url:
+            yield scrapy.Request(next_page_url, callback=self.parse)
+
+
+    def get_next_page(self, response):
+        if '?page=' in response.url:
+            next_page = 2
+        else:
+            current_page = int(response.url.split('page=')[-1])
+            next_page = current_page + 1
+        
+        next_page_url = f'{self.start_urls}?page={next_page}'
+
+        return next_page_url
+    
+    
