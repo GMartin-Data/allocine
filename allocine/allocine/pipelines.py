73032022a7d6efb062f8a687cc5aaf25f154d761
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-29 16:27:46 +0100

    Implement duration conversion

--- allocine/allocine/pipelines.py
+++ allocine/allocine/pipelines.py
@@ -5,6 +5,8 @@
 
 
 # useful for handling different item types with a single interface
+from typing import Optional
+
 from itemadapter import ItemAdapter
 from loguru import logger
 
@@ -12,6 +14,16 @@ from loguru import logger
 SET_FIELDS = ("genres", "casting")
 INTEGER_FIELDS = ("film_id", "entries")
 FLOAT_FIELDS = ("press_ratings", "viewers_ratings")
+
+
+def convert_duration(duration: str) -> Optional[int]:
+    """Convert a duration string to its minutes counterpart"""
+    if duration is None or not duration.endswith("min"):
+        return None
+    duration = duration.split()
+    hours = int(duration[0].replace("h", ""))
+    minutes = int(duration[1].replace("min", ""))
+    return (60 * hours + minutes)
 
 class CleanPipeline:
     @logger.catch
@@ -41,5 +53,12 @@ class CleanPipeline:
                 adapter[field] = "|".join(value)
             except BaseException as e:
                 adapter[field] = None
-            
+
+        # Special case of duration
+        duration = adapter.get("duration")
+        if duration is not None:
+            adapter["duration"] = convert_duration(duration)
+        else:
+            adapter["duration"] = None
+   
         return item
