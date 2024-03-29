99819370ab8c2357a1c3beda3f545d344c3db625
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-29 15:55:35 +0100

    Pipeline without duration handling

--- allocine/allocine/pipelines.py
+++ allocine/allocine/pipelines.py
@@ -6,8 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from loguru import logger
 
 
-class AllocinePipeline:
+SET_FIELDS = ("genres", "casting")
+INTEGER_FIELDS = ("film_id", "entries")
+FLOAT_FIELDS = ("press_ratings", "viewers_ratings")
+
+class CleanPipeline:
+    @logger.catch
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        for field in FLOAT_FIELDS:
+            value = adapter.get(field)
+            if value not in ("--", None):
+                value = value.replace(",", ".")
+                adapter[field] = float(value)
+            else:
+                adapter[field] = None
+
+        for field in INTEGER_FIELDS:
+            value = adapter.get(field)
+            if value not in ("--", None):
+                value = value.replace(" ", "")
+                adapter[field] = int(value)
+            else:
+                adapter[field] = None
+
+        for field in SET_FIELDS:
+            value = adapter.get(field)
+            try:
+                value = set(item.strip() for item in value)
+                adapter[field] = "|".join(value)
+            except BaseException as e:
+                adapter[field] = None
+            
         return item
